863ff364ffb86d746471c226e247672f4da1b7e5
David Silva <Davidslv@users.noreply.github.com>
2022-05-21 16:25:41 +0100

    Add PostcodeSearches

---
+++ app/controllers/postcode_searches_controller.rb
@@ -0,0 +1,6 @@
+class PostcodeSearchesController < ApplicationController
+  def index; end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :postcode_searches, only: %i(index create)
+
   namespace :admin do
     resources :allowed_areas
     resources :allowed_postcodes
